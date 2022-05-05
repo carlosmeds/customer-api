40ce0122119d68888332e354e4362772c7a11556
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-05 14:29:57 -0300

    test: check customerRepository to add user

--- src/use-cases/customer-uc.spec.ts
+++ src/use-cases/customer-uc.spec.ts
@@ -1,10 +1,21 @@
+import { Customer } from '../domain/customer';
 import { CustomerUC } from './customer-uc';
+
+class inMemoryCustomerRepository {
+  async addCustomer(): Promise<Customer> {
+    const customer = new Customer('uuid', 123456789, 'John Doe');
+    return customer;
+  }
+}
 
 describe('Customer Use Case', () => {
   it('should add customer', async () => {
-    const customer = new CustomerUC('teste');
-    const teste = await customer.addCustomer();
+    const customerRepository = new inMemoryCustomerRepository();
+    const customerUC = new CustomerUC(customerRepository);
+    const customerTest = new Customer('uuid', 123456789, 'John Doe');
 
-    expect(teste).toBe('customer');
+    const addedCustomer = await customerUC.addCustomer();
+
+    expect(addedCustomer.id).toBe(customerTest.id);
   });
 });
