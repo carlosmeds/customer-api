7ef23d33c1952dc0fdd853d533c6c371b30eae44
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-06 10:30:51 -0300

    feat: add updateCustomer

--- src/presentation/controllers/client-controller.ts
+++ src/presentation/controllers/client-controller.ts
@@ -6,6 +6,7 @@ import {
   Get,
   Param,
   HttpCode,
+  Put,
 } from '@nestjs/common';
 import { RedisCustomerRepository } from 'src/infra/redis';
 import { CustomerUC } from 'src/use-cases/customer/customer-uc';
@@ -24,6 +25,15 @@ export class CustomerController {
 
   @Get('customers/:id')
   async getCustomer(@Res() response, @Param('id') id: string): Promise<any> {
+    const customerRepository = new RedisCustomerRepository();
+    const customerUc = new CustomerUC(customerRepository);
+    const customer = await customerUc.getCustomer(id);
+
+    return response.status(200).send(customer.name);
+  }
+
+  @Put('customers/:id')
+  async updateCustomer(@Res() response, @Param('id') id: string): Promise<any> {
     const customerRepository = new RedisCustomerRepository();
     const customerUc = new CustomerUC(customerRepository);
     const customer = await customerUc.getCustomer(id);
--- src/use-cases/customer/customer-uc.spec.ts
+++ src/use-cases/customer/customer-uc.spec.ts
@@ -5,6 +5,9 @@ class inMemoryCustomerRepository {
   async addCustomer(customer: Customer): Promise<Customer> {
     return customer;
   }
+  async getCustomer(id: string): Promise<Customer> {
+    return new Customer('uuid', 123456789, 'John Doe');
+  }
 }
 
 describe('Customer Use Case', () => {
@@ -14,7 +17,16 @@ describe('Customer Use Case', () => {
     const customerTest = new Customer('uuid', 123456789, 'John Doe');
 
     const addedCustomer = await customerUC.addCustomer(123456789, 'John Doe');
-    console.log(addedCustomer);
+
+    expect(addedCustomer.name).toBe(customerTest.name);
+    expect(addedCustomer.document).toBe(customerTest.document);
+  });
+  it('should get customer', async () => {
+    const customerRepository = new inMemoryCustomerRepository();
+    const customerUC = new CustomerUC(customerRepository);
+    const customerTest = new Customer('uuid', 123456789, 'John Doe');
+
+    const addedCustomer = await customerUC.getCustomer('uuid');
 
     expect(addedCustomer.name).toBe(customerTest.name);
     expect(addedCustomer.document).toBe(customerTest.document);
