f1a5101377615bc4e55203fc01f59708eb3d8a00
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-06 11:22:29 -0300

    feat: update protocols

--- src/use-cases/customer/protocols.ts
+++ src/use-cases/customer/protocols.ts
@@ -3,4 +3,5 @@ import { Customer } from '../../domain/customer';
 export interface ICustomer {
   addCustomer(document: number, name: string): Promise<Customer>;
   getCustomer(id: string): Promise<Customer>;
+  updateCustomer(customer: Customer): Promise<Customer>;
 }
