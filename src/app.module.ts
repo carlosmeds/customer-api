70b1e22b81ef4f52b8874d452ba0f2779c4b37e7
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-06 10:58:41 -0300

    refactor: update customer controller name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { CustomerController } from 'src/presentation/controllers/client-controller';
+import { CustomerController } from 'src/presentation/controllers/customer-controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
