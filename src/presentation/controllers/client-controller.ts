c2b0318a5e31bf5a58a7066930636e3d1649d29c
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-05 23:34:33 -0300

    feat: add http status on decorator

--- src/presentation/controllers/client-controller.ts
+++ src/presentation/controllers/client-controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Post, Res, Body, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Res,
+  Body,
+  Get,
+  Param,
+  HttpCode,
+} from '@nestjs/common';
 import { RedisCustomerRepository } from 'src/infra/redis';
 import { CustomerUC } from 'src/use-cases/customer/customer-uc';
 
 @Controller()
 export class CustomerController {
   @Post('customers')
+  @HttpCode(201)
   async addCustomer(@Res() response, @Body() body): Promise<any> {
     const customerRepository = new RedisCustomerRepository();
     const customerUc = new CustomerUC(customerRepository);
     const customer = await customerUc.addCustomer(body?.document, body?.name);
 
-    return response.status(201).send(customer.id);
+    return response.send(customer.id);
   }
 
   @Get('customers/:id')
