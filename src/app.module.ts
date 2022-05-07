a23c06ae27c59d3fbc2acfe09f7a905ed3de7ba8
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-07 10:55:38 -0300

    fix: add nest config to use dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomerController } from 'src/presentation/controllers/customer-controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, CustomerController],
   providers: [AppService],
 })
