c5f53cfae82eda196810317d46fef9994a51c24d
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-07 11:06:26 -0300

    fix: adjust dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { CustomerController } from 'src/presentation/controllers/customer-controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [],
   controllers: [AppController, CustomerController],
   providers: [AppService],
 })
--- src/main/env.ts
+++ src/main/env.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/../../.env` });
+
 export default {
   redisHost: process.env.REDIS_HOST || 'localhost',
   redisPort: process.env.REDIS_PORT || 6379,
