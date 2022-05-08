4cce1643262c242c7516810fc1ad45ce6e695bcc
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-08 19:50:28 -0300

    refactor: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CustomerController } from 'src/presentation/controllers/customer-controller';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthMiddleware } from './presentation/middlewares/auth-middleware';
 
 @Module({
   imports: [],
-  controllers: [AppController, CustomerController],
-  providers: [AppService],
+  controllers: [CustomerController],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
