40f66197ddf9d5c4992ff152562f766362b60abf
Carlos Nakaguishi <carlosmedeiros964@gmail.com>
2022-05-09 23:08:43 -0300

    refactor: remove unused protocol

--- src/use-cases/customer/protocols.ts
+++
@@ -1,7 +0,0 @@
-import { Customer } from '../../domain/customer';
-
-export interface ICustomer {
-  addCustomer(document: number, name: string): Promise<Customer>;
-  getCustomer(id: string): Promise<Customer>;
-  updateCustomer(customer: Customer): Promise<Customer>;
-}
